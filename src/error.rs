7b206781582d7d0d9e1077c14dc0923b3753d956
actuallylost <code@lostcave.dev>
2025-08-22 12:20:16 +0300

    feat: add helper Error methods

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,17 @@
+use crate::token::Token;
+
 #[derive(Debug)]
 pub enum Error {
-    ParseError(&'static str),
+    ParseError(String),
     UnknownCharacterError(char),
+}
+
+impl Error {
+    pub fn from_str(err: &str) -> Error {
+        Error::ParseError(format!("{}", err))
+    }
+
+    pub fn from_token(err: &str, token: &Token) -> Error {
+        Error::ParseError(format!("{}: {:?}", err, token))
+    }
 }
