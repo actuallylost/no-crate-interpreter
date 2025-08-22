53f893d7811e5a996b7b2ff14dd8f2eaf700e6fe
actuallylost <code@lostcave.dev>
2025-08-22 18:04:06 +0300

    feat: update Error variants; add TokenType; add Display impl for Error & TokenType; add std::Error impl for Error

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,51 @@
+use std::fmt::{Display, Formatter, Result};
+
 use crate::token::Token;
 
 #[derive(Debug)]
 pub enum Error {
-    ParseError(String),
-    UnknownCharacterError(char),
+    // Lexer Errors
+    UnexpectedCharacter(char),
+    // Parser Errors
+    EmptyTokens,
+    UnexpectedToken(TokenType, Token, usize),
+    UnexpectedEndOfLine(char),
 }
 
-impl Error {
-    pub fn from_str(err: &str) -> Error {
-        Error::ParseError(format!("{}", err))
-    }
+#[derive(Debug)]
+pub enum TokenType {
+    Plus,
+    Minus,
+    Mul,
+    Div,
+    Mod,
+    Lit,
+}
 
-    pub fn from_token(err: &str, token: &Token) -> Error {
-        Error::ParseError(format!("{}: {:?}", err, token))
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Error::UnexpectedCharacter(c) => write!(f, "Found unexpected character: {}", c),
+            Error::EmptyTokens => write!(f, "Expected at least 1 token, found 0",),
+            Error::UnexpectedToken(exp, fnd, pos) => {
+                write!(f, "Expected {}, found {} at pos {}", exp, fnd, pos)
+            }
+            Error::UnexpectedEndOfLine(c) => write!(f, "Expected \\n found {}", c),
+        }
     }
 }
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            TokenType::Plus => write!(f, "Plus"),
+            TokenType::Minus => write!(f, "Minus"),
+            TokenType::Mul => write!(f, "Mul"),
+            TokenType::Div => write!(f, "Div"),
+            TokenType::Mod => write!(f, "Mod"),
+            TokenType::Lit => write!(f, "Lit"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
