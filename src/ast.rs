4f0f69307565e7fe2d54c8b16caf087ab37a49d0
actuallylost <code@lostcave.dev>
2025-08-26 19:48:58 +0300

    feat: add and implement Ast; add Stmt enum; add EOF to Expr enum

--- src/ast.rs
+++ src/ast.rs
@@ -1,19 +1,32 @@
-use crate::{error::Error, parser::ParserState};
-
 #[derive(Debug)]
-pub struct Ast {}
+pub struct Ast(Vec<Stmt>);
 
 impl Ast {
     pub fn new() -> Self {
-        todo!();
+        Self(Vec::new())
+    }
+
+    pub fn push(&mut self, val: Stmt) {
+        self.0.push(val)
+    }
+
+    pub fn _pop(&mut self) -> Option<Stmt> {
+        self.0.pop()
     }
 }
 
+#[derive(Debug)]
+pub enum Stmt {
+    Expr(Box<Expr>),
+}
+
 /// Defines all Expression variants
+#[derive(Debug)]
 pub enum Expr {
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
     Mul(Box<Expr>, Box<Expr>),
     Div(Box<Expr>, Box<Expr>),
     Lit(i32),
+    EOF,
 }
