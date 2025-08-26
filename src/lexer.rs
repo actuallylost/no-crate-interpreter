d5536d5339ccd17f5ff847efa14452b3871e2aa6
actuallylost <code@lostcave.dev>
2025-08-26 21:16:14 +0300

    tests(lexer): add lexer tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -70,3 +70,136 @@ impl Lexer {
         Ok(tokens)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn single_char_literals() {
+        let lexer = Lexer::new(String::from("1 2 5 8 9 1 7"));
+        let expected = vec![
+            Token::Lit(1),
+            Token::Lit(2),
+            Token::Lit(5),
+            Token::Lit(8),
+            Token::Lit(9),
+            Token::Lit(1),
+            Token::Lit(7),
+        ];
+        let actual = lexer.tokenize().unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn single_char_literals_leading_zero() {
+        let lexer = Lexer::new(String::from("01 02 05 08 09 01 07"));
+        let expected = vec![
+            Token::Lit(1),
+            Token::Lit(2),
+            Token::Lit(5),
+            Token::Lit(8),
+            Token::Lit(9),
+            Token::Lit(1),
+            Token::Lit(7),
+        ];
+        let actual = lexer.tokenize().unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn multi_char_literals() {
+        let lexer = Lexer::new(String::from("12 333 81 741 32"));
+        let expected = vec![
+            Token::Lit(12),
+            Token::Lit(333),
+            Token::Lit(81),
+            Token::Lit(741),
+            Token::Lit(32),
+        ];
+        let actual = lexer.tokenize().unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn multi_char_literals_leading_zero() {
+        let lexer = Lexer::new(String::from("012 0333 081 0741 032"));
+        let expected = vec![
+            Token::Lit(12),
+            Token::Lit(333),
+            Token::Lit(81),
+            Token::Lit(741),
+            Token::Lit(32),
+        ];
+        let actual = lexer.tokenize().unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn single_char_identifiers() {
+        let lexer = Lexer::new(String::from("a B g o L p K m T D"));
+        let expected = vec![
+            Token::Ident(String::from("a")),
+            Token::Ident(String::from("B")),
+            Token::Ident(String::from("g")),
+            Token::Ident(String::from("o")),
+            Token::Ident(String::from("L")),
+            Token::Ident(String::from("p")),
+            Token::Ident(String::from("K")),
+            Token::Ident(String::from("m")),
+            Token::Ident(String::from("T")),
+            Token::Ident(String::from("D")),
+        ];
+        let actual = lexer.tokenize().unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn multi_char_identifiers() {
+        let lexer = Lexer::new(String::from(
+            "awd BJUD gasd odus LaU pUds KadU maDp TaUID Ddua",
+        ));
+        let expected = vec![
+            Token::Ident(String::from("awd")),
+            Token::Ident(String::from("BJUD")),
+            Token::Ident(String::from("gasd")),
+            Token::Ident(String::from("odus")),
+            Token::Ident(String::from("LaU")),
+            Token::Ident(String::from("pUds")),
+            Token::Ident(String::from("KadU")),
+            Token::Ident(String::from("maDp")),
+            Token::Ident(String::from("TaUID")),
+            Token::Ident(String::from("Ddua")),
+        ];
+        let actual = lexer.tokenize().unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn symbols() {
+        let lexer = Lexer::new(String::from("+ - * / % = ( ) [ ] { }"));
+        let expected = vec![
+            Token::Plus,
+            Token::Minus,
+            Token::Mul,
+            Token::Div,
+            Token::Mod,
+            Token::Equals,
+            Token::LParen,
+            Token::RParen,
+            Token::LBracket,
+            Token::RBracket,
+            Token::LBrace,
+            Token::RBrace,
+        ];
+        let actual = lexer.tokenize().unwrap();
+
+        assert_eq!(expected, actual);
+    }
+}
